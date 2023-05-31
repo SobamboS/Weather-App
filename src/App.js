9e5279ea122574899716e6523cbd74486cb75f7d
SobamboS <sobamboolusola1152@gmail.com>
2023-05-31 10:36:47 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const KEY = process.env.API_KEY;
+    axios
+      .get(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`)
+      .then((response) => {
+        setWeatherData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{backgroundColor:"black"}}>
+      <a href="https://www.yahoo.com/?ilc=401" target="_blank">
+         <img src="https://poweredby.yahoo.com/poweredby_yahoo_v_white.png" width="100" height="100"/> </a>
+      </div>
+      <h1>Weather Application</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+      {loading ? (
+        <p>Loading...</p>
+      ) : weatherData ? (
+        <div>
+          <h2>{weatherData.location.name}</h2>
+          <p>Temperature: {weatherData.current.temp_c}°C</p>
+          <p>Condition: {weatherData.current.condition.text}</p>
+        </div>
+      ) : null}
     </div>
   );
 }
 
 export default App;
+
